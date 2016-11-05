f8a13820ed6bc1063696cc27c1481509e5efddeb
tlin108 <tlin108@hotmail.com>
2016-11-04 20:57:01 -0400

    start using component structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
+import { Grid, Image } from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
