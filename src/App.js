7ba96fa43e4788a3ce837142a010f44669e7d5e2
tlin108 <tlin108@hotmail.com>
2016-11-04 23:11:41 -0400

    semi complete LogoHeader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { Grid, Image } from 'semantic-ui-react';
 import './App.css';
+
+import LogoHeader from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <LogoHeader />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
