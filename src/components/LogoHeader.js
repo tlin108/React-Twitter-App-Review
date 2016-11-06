4bbcad897f5253428268cd57f5d4d63797928395
tlin108 <tlin108@hotmail.com>
2016-11-05 22:13:26 -0400

    banner minor fix

--- src/components/LogoHeader.js
+++ src/components/LogoHeader.js
@@ -9,8 +9,8 @@ export default class LogoHeader extends Component {
           <Grid.Column width={2}>
             <Image src='http://www.pngall.com/wp-content/uploads/2016/07/Twitter-Download-PNG.png' />
           </Grid.Column>
-          <Grid.Column width={6}>
-            <Header as='h1'>Reviews for Twitter</Header>
+          <Grid.Column verticalAlign='middle' width={8}>
+            <Header as='h1' className='BannerTitle'>Reviews for Twitter</Header>
           </Grid.Column>
         </Grid>
       </Container>
