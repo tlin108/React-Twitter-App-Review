c47abee4addb27164b297f215eb8f854c19ef226
tlin108 <tlin108@hotmail.com>
2016-12-10 23:47:29 -0500

    adjust to style guide's filename and extension

--- src/components/LoadMoreReviews.jsx
+++ src/components/LoadMoreReviews.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
 
-const loadMoreReviews = (props) => {
+function loadMoreReviews(props) {
   if(props.isLoading || props.hasError){
     return null;
   }
